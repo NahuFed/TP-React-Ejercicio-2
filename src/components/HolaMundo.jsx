9a16f17f5294b261fe3eadf05dfcb800db6f7e66
Nahuel Rodriguez <nahufed@gmail.com>
2023-05-28 20:51:44 -0300

    enviado por props el receptor del mensaje al componente hijo

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HolaMundo = (props) => {
+    return (
+        <div>
+            <h1>Hello {props.receptor}!</h1>
+        </div>
+    );
+};
+
+export default HolaMundo;
